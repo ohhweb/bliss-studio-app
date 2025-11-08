f63ca54880bbd82a14a9082092f6a06c97fbdd4e
ohhweb <177416367+ohhweb@users.noreply.github.com>
2025-11-08 22:51:05 +0530

    error fixed

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <form method="POST" action="{{ route('categories.update', $category) }}">
+                    <form method="POST" action="{{ route('admin.categories.update', $category) }}">
                         @csrf
                         @method('PUT')
 
@@ -22,7 +22,7 @@
 
                         <!-- Submit Button -->
                         <div class="flex items-center justify-end mt-4">
-                             <a href="{{ route('categories.index') }}" class="text-gray-600 hover:text-gray-900 mr-4">Cancel</a>
+                             <a href="{{ route('admin.categories.index') }}" class="text-gray-600 hover:text-gray-900 mr-4">Cancel</a>
                             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 Update Category
                             </button>
